13767d16d09e37b9f12ee8f440f0c46fcbff3931
leeyunhome <leeyunhome@daum.net>
2020-03-31 21:49:11 +0900

    4-1 React 조건문

--- client.jsx
+++ client.jsx
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root'
 
-import 
+import  ResponseCheck from './ResponseCheck';
+
+const Hot = hot(ResponseCheck);
+
+ReactDom.render(<Hot />, document.querySelector('#root'));
