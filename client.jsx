8f15c645ac5da6be0e82bd4b3bbb623c020f629c
leeyunhome <leeyunhome@daum.net>
2020-04-01 12:43:18 +0900

    5-1 react lifecycle introduction

--- client.jsx
+++ client.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root'
 
-import  ResponseCheck from './ResponseCheck';
+import  RSP from './RSP';
 
-const Hot = hot(ResponseCheck);
+const Hot = hot(RSP);
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
