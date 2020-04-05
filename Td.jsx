6f21e7332b4dc86902b9c9ae32d6547946a9db77
leeyunhome <leeyunhome@daum.net>
2020-04-05 17:08:29 +0900

    7-4 틱택토 구현

--- Td.jsx
+++ Td.jsx
@@ -1,12 +1,23 @@
-import React, { useCallback } from 'react';
-import { CLICK_CELL, CHANGE_TURN } from './TicTacToe';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { CLICK_CELL } from './TicTacToe';
 
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
+    console.log('td rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3]);
+        ref.current = [rowIndex, cellIndex, dispatch, cellData];
+    }, [rowIndex, cellIndex, dispatch, cellIndex]);
+
     const onClickTd = useCallback(() => {
         console.log(rowIndex, cellIndex);
+        if (cellData) {
+            return;
+        }
         dispatch({type: CLICK_CELL, row: rowIndex, cell: cellIndex });
-        dispatch({type: CHANGE_TURN});
-    }, []);
+        // dispatch({type: CHANGE_TURN});
+    }, [cellData]);
 
     return (
         <td onClick={onClickTd}>{cellData}</td>
